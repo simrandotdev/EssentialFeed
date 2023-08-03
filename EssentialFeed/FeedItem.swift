02de602cd2a4e998e685d45504d7caa3481800e9
Simran Preet Singh <simran5590@gmail.com>
2023-08-03 08:47:18 -0700

    Create the FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,16 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Simran Preet Narang on 2023-08-03.
+//
+
+import Foundation
+
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
